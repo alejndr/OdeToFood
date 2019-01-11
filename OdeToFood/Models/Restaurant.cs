3c245e66e28c9972724e2c13dd5695670a6f96bf
I. la Maza Villalba <i.la.maza.villalba@avanade.com>
2019-01-11 12:26:52 +0100

    Added Entity Framework

--- OdeToFood/Models/Restaurant.cs
+++ OdeToFood/Models/Restaurant.cs
@@ -13,6 +13,7 @@ namespace OdeToFood.Models
 		[Display(Name="Restaurant Name")]
 		[Required, MaxLength(80)]
 		public string Name { get; set; }
+
 		public CuisineType Cuisine { get; set; }
 	}
 }
